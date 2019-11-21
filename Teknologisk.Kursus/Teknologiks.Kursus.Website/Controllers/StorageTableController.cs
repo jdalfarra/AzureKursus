857083fcda88c6750dd25ef830f8fb5d4ba9f5e2
jdalfarra <jonasdalle@gmail.com>
2019-11-21 14:27:02 +0100

    added Table blob and index.cshtml

---
+++ Teknologisk.Kursus/Teknologiks.Kursus.Website/Controllers/StorageTableController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Teknologiks.Kursus.Website.Models;
+
+namespace Teknologiks.Kursus.Website.Controllers
+{
+    public class StorageTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            const string storageConnectionString = "https://mainvaultjdk.vault.azure.net/secrets/StorageAccountConnection";
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient =
+                new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var tableConnectionString = keyVaultClient.GetSecretAsync(storageConnectionString).GetAwaiter().GetResult().Value;
+
+            var cloudStorageAccount = CloudStorageAccount.Parse(tableConnectionString);
+            CloudTableClient cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
+            CloudTable customersTable = cloudTableClient.GetTableReference("customers");
+            customersTable.CreateIfNotExists();
+
+            TableQuery<Customer> query = new TableQuery<Customer>();
+
+            var customers = customersTable.ExecuteQuery(query).ToList();
+
+            return View(customers);
+        }
+    }
+}
---
+++ Teknologisk.Kursus/Teknologiks.Kursus.Website/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Teknologiks.Kursus.Website.Models
+{
+    public class Customer : TableEntity
+    {
+        public Customer(string area, string customerNumber)
+        {
+            PartitionKey = area;
+            RowKey = customerNumber;
+        }
+
+        public Customer()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Category { get; set; }
+        
+    }
+}
